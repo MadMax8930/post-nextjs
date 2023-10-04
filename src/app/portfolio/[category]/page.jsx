51582a0074184d2b3dec08121e40f5fb3f61862a
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-10-04 16:35:40 +0200

    category data map

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,26 +1,41 @@
 import React from 'react'
 import Image from 'next/image'
 import Button from '@/components/button/Button'
+import { items } from './data.js'
+import { notFound } from 'next/navigation'
 import styles from './page.module.css'
+
+const getData = (cat) => {
+   const data = items[cat]
+   if (data) { 
+      return data;
+   } else {
+      return notFound();
+   }
+}
 
 const Category = ({ params }) => {
    console.log(params)
+   const data = getData(params.category)
+   
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <div className={styles.item}>
-         <div className={styles.content}>
-            <h1 className={styles.title}>Test</h1>
-            <p className={styles.desc}>Desc</p>
-            <Button url="#" text="See More" />
+      {data.map((item) => (
+         <div className={styles.item} key={item.id}>
+            <div className={styles.content}>
+               <h1 className={styles.title}>{item.title}</h1>
+               <p className={styles.desc}>{item.desc}</p>
+               <Button url="#" text="See More" />
+            </div>
+            <div className={styles.imgContainer}>
+               <Image
+                  src={item.image} alt="Category"
+                  fill={true} className={styles.img}
+               />
+            </div>   
          </div>
-         <div className={styles.imgContainer}>
-            <Image
-               src={item.image} alt="Category"
-               fill={true} className={styles.img}
-            />
-         </div>      
-      </div>
+      ))}
    </div>
   )
 }
