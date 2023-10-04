65cccdbedcadb7b5cfc6ec3aa1c6f79799496356
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-10-04 16:35:18 +0200

    swr hook

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,26 +1,31 @@
 "use client"
 import React, { useState, useEffect } from 'react'
+import useSWR from 'swr';
 import styles from './page.module.css'
 
 const Dashboard = () => {
-   const [data, setData] = useState([]);
-   const [err, setErr] = useState(false);
-   const [isLoading, setIsLoading] = useState(false);
+   // const [data, setData] = useState([]);
+   // const [err, setErr] = useState(false);
+   // const [isLoading, setIsLoading] = useState(false);
 
-   useEffect(() => {
-      const getData= async () => {
-         setIsLoading(true);
-         const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
-            cache: 'no-store'
-         });
-         if (!res.ok) setErr(true);
+   // useEffect(() => {
+   //    const getData= async () => {
+   //       setIsLoading(true);
+   //       const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+   //          cache: 'no-store'
+   //       });
+   //       if (!res.ok) setErr(true);
       
-         const data = await res.json();
-         setData(data);
-         setIsLoading(false);
-      };
-      getData();
-   }, []);
+   //       const data = await res.json();
+   //       setData(data);
+   //       setIsLoading(false);
+   //    };
+   //    getData();
+   // }, []);
+
+   const fetcher = (url) => fetch(url).then(res => res.json());
+   const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher);
+   
 
    console.log(data);
 
