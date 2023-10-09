a5fd7289edeb8b6e54353540d9ad8efc7dee1e12
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-10-09 14:10:54 +0200

    next register done

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -2,12 +2,15 @@
 import React from 'react'
 import { signIn } from 'next-auth/react'
 import styles from './page.module.css'
-import Google from 'next-auth/providers/google'
 
 const Login = () => {
+   const handleGoogleSignIn = async () => {
+      await signIn("google")
+   };
+
   return (
     <div className={styles.container}>
-      <button onClick={() => signIn(Google)}>Login with Google</button>
+      <button onClick={handleGoogleSignIn}>Login with Google</button>
     </div>
   )
 }
--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import styles from './page.module.css'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 const Register = () => {
+   const [err, setErr] = useState(false);
+   const router = useRouter();
+
+   const handleRegister = async (e) => {
+      e.preventDefault();
+      // Could use useState
+      const name = e.target[0].value;
+      const email = e.target[1].value;
+      const password = e.target[2].value;
+
+      try {
+         const res = await fetch('/api/auth/register', {
+            method: "POST",
+            headers: {
+               "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ name, email, password })
+         });
+
+         res.status === 201 && router.push("/dashboard/login?success=Account has been created");
+      } catch(err) {
+         setErr(true);
+      }
+   }
   return (
-    <div>Register</div>
+    <div className={styles.container}>
+      <form onSubmit={handleRegister} className={styles.form}>
+         <input type="text" placeholder="username" className={styles.input} required />
+         <input type="email" placeholder="email" className={styles.input} required />
+         <input type="password" placeholder="password" className={styles.input} required />
+         <button className={styles.button}>Register</button>
+      </form>
+      {err && "Something went wrong!"}
+      <Link href={"/dashboard/login"}>Login with an existing account</Link>
+    </div>
   )
 }
 
